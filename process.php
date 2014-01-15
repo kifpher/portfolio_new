8bf79a8f7047f3db470ca20c04fadb01d93a5d5b
Jayden Anderson <jaydencanderson@gmail.com>
2014-01-14 20:55:24 -0800

    Adding working form.

--- process.php
+++ process.php
@@ -1,13 +1,19 @@
 <?php
-// Get Data	
-$name = strip_tags($_POST['name']);
-$email = strip_tags($_POST['email']);
-$phone = strip_tags($_POST['phone']);
-$url = strip_tags($_POST['url']);
-$message = strip_tags($_POST['message']);
-
-// Send Message
-mail( "yournameeeee@gmail.com", "Contact Form Submission",
-"Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $url\nMessage: $message\n",
-"From: Forms <forms@example.net>" );
+ 
+    $to = "mrkita.reece@gmail.com"; 
+    $from = $_REQUEST['email']; 
+    $name = $_REQUEST['name']; 
+    $headers = "From: $from"; 
+    $subject = "You have a message sent from your site"; 
+ 
+    $fields = array(); 
+    $fields{"name"} = "name"; 
+    $fields{"email"} = "email"; 
+    $fields{"phone"} = "phone"; 
+    $fields{"message"} = "message";
+ 
+    $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+ 
+    $send = mail($to, $subject, $body, $headers);
+ 
 ?>
